3a9feb4dbf2ec59ecfc0d740afa21e99eed06b1a
uday <budaysaireddy@gmail.com>
2023-07-16 10:26:46 +0530

    k

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // Update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.listen(PORT, () => {
     console.log("Listening on PORT:" + PORT);
